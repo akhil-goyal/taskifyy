97d24f8caa2d1673c4b84e0239abebb0003d3aa3
Akhil Goyal <akhilgoyal@Akhils-MacBook-Pro.local>
2025-04-03 11:45:12 -0400

    server config after mongodb connection

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,19 +1,32 @@
-const express = require("express");
-const mongoose = require("mongoose");
+import express from "express";
+import mongoose, { ConnectOptions } from "mongoose";
+import { createServer } from "http";
+import { Server } from "socket.io";
 
-const PORT: number = 4000;
+const PORT: number = 4001;
 const dbString: string =
   "mongodb+srv://akhilgoyalwork:v8NscXcZWxKO2ouh@taskifyy.2auuizl.mongodb.net/?retryWrites=true&w=majority&appName=Taskifyy";
 
 const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer);
 
-app.listen(PORT, () => {
-  console.log(`Server is running at PORT: ${PORT}`);
+app.get("/", (req, res) => {
+  res.send("API is working");
+});
+
+io.on("connection", () => {
+  console.log("Socket connected!");
 });
 
 mongoose
-  .connect(dbString, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("MongoDB Connected!"))
+  .connect(dbString)
+  .then(() => {
+    console.log("MongoDB Connected!");
+    httpServer.listen(PORT, () => {
+      console.log(`Server running on PORT: ${PORT}`);
+    });
+  })
   .catch((err: Error) => {
     console.error(err);
   });
