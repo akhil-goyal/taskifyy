a8bd6f7f2816bb63cab7b38284d1f35b045f3048
Akhil Goyal <akhilgoyal@Akhils-MacBook-Pro.local>
2025-04-03 10:53:28 -0400

    mongodb setup

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,19 @@
-console.log("server");
+const express = require("express");
+const mongoose = require("mongoose");
+
+const PORT: number = 4000;
+const dbString: string =
+  "mongodb+srv://akhilgoyalwork:v8NscXcZWxKO2ouh@taskifyy.2auuizl.mongodb.net/?retryWrites=true&w=majority&appName=Taskifyy";
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`Server is running at PORT: ${PORT}`);
+});
+
+mongoose
+  .connect(dbString, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB Connected!"))
+  .catch((err: Error) => {
+    console.error(err);
+  });
