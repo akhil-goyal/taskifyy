97d10d4b779f90e0624f94cdf03c4b6a6593609f
Akhil Goyal <akhilgoyal@Akhils-MacBook-Pro.local>
2025-04-10 17:53:22 -0400

    create inline form

---
+++ client/src/app/shared/components/inlineForm/inlineForm.component.ts
@@ -0,0 +1,41 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'inline-form',
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './inlineForm.component.html',
+})
+export class InlineFormComponent {
+  @Input() title: string = '';
+  @Input() defaultText: string = 'Not defined';
+  @Input() hasButton: boolean = false;
+  @Input() buttonText: string = 'Submit';
+  @Input() inputPlaceholder: string = '';
+  @Input() inputType: string = 'input';
+
+  @Output() handleSubmit = new EventEmitter<string>();
+
+  isEditing: boolean = false;
+  form = this.fb.group({
+    title: [''],
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  activeEditing(): void {
+    if (this.title) {
+      this.form.patchValue({ title: this.title });
+    }
+    this.isEditing = true;
+  }
+
+  onSubmit(): void {
+    if (this.form.value.title) {
+      this.handleSubmit.emit(this.form.value.title);
+    }
+    this.isEditing = false;
+    this.form.reset();
+  }
+}
