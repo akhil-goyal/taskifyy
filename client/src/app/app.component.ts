fdb6220cf7277d36bf3b9beb59a03fe4034b2b67
Akhil Goyal <akhilgoyal@Akhils-MacBook-Pro.local>
2025-04-10 14:10:33 -0400

    test commit

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { AuthService } from './auth/services/auth.service';
+import { RegisterComponent } from './auth/components/register/register.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, RegisterComponent],
+  templateUrl: './app.component.html',
+  providers: [AuthService],
+})
+export class AppComponent implements OnInit {
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.authService.getCurrentUser().subscribe({
+      next: (res) => {
+        console.log('res', res);
+      },
+      error: (err) => {
+        console.log('err', err);
+        this.authService.setCurrenUser(null);
+      },
+    });
+  }
+}
---
+++ client/src/app/auth/components/register/register.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'auth-register',
+  templateUrl: './register.component.html',
+})
+export class RegisterComponent {}
