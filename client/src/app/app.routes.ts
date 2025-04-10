8c8101db038a2832d5a8191c7558014d56ca5fb7
Akhil Goyal <akhilgoyal@Akhils-MacBook-Pro.local>
2025-04-10 15:36:43 -0400

    implement home component

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './auth/components/register/register.component';
 import { LoginComponent } from './auth/components/login/login.component';
+import { HomeComponent } from './home/components/home/home.component';
 
 export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
   {
     path: 'register',
     component: RegisterComponent,
