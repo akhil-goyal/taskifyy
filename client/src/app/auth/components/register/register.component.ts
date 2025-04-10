94b6c69b6b13de06bea6b482f392fc74dadaa43d
Akhil Goyal <akhilgoyal@Akhils-MacBook-Pro.local>
2025-04-10 14:44:23 -0400

    add reactive form for registeration component

--- client/src/app/auth/components/register/register.component.ts
+++ client/src/app/auth/components/register/register.component.ts
@@ -1,7 +1,26 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'auth-register',
+  imports: [ReactiveFormsModule, FormsModule],
   templateUrl: './register.component.html',
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  form = this.fb.group({
+    email: ['', Validators.required],
+    username: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  onSubmit(): void {
+    console.log('onSubmit', this.form.value);
+  }
+}
