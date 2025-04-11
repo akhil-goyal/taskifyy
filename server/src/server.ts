4656712eda142b5a3c530cd5f5ec8b4273c0751f
Akhil Goyal <akhilgoyal@Akhils-MacBook-Pro.local>
2025-04-11 10:41:23 -0400

    implement get columns endpoint

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,6 +6,7 @@ import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
 import * as usersController from "./controllers/users";
 import * as boardsController from "./controllers/boards";
+import * as columnsController from "./controllers/columns";
 import bodyParser from "body-parser";
 import authMiddleware from "./middlewares/auth";
 import cors from "cors";
@@ -41,6 +42,11 @@ app.post("/api/users/login", usersController.login);
 app.get("/api/user", authMiddleware, usersController.currentUser);
 app.get("/api/boards", authMiddleware, boardsController.getBoards);
 app.get("/api/boards/:boardId", authMiddleware, boardsController.getBoard);
+app.get(
+  "/api/boards/:boardId/columns",
+  authMiddleware,
+  columnsController.getColumns
+);
 app.post("/api/boards", authMiddleware, boardsController.createBoard);
 
 io.use(async (socket: Socket, next) => {
