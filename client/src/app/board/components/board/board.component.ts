861e4c71373b70965cded4d5ae55e36cc0f3e96b
Akhil Goyal <akhilgoyal@Akhils-MacBook-Pro.local>
2025-04-11 10:46:12 -0400

    implement column retrieval on client side

--- client/src/app/board/components/board/board.component.ts
+++ client/src/app/board/components/board/board.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, NavigationStart, Router } from '@angular/router';
-import { filter, Observable } from 'rxjs';
+import { combineLatest, filter, map, Observable } from 'rxjs';
 import { BoardsService } from 'src/app/shared/services/boards.service';
+import { ColumnsService } from 'src/app/shared/services/columns.service';
 import { SocketService } from 'src/app/shared/services/socket.service';
 import { BoardInterface } from 'src/app/shared/types/board.interface';
+import { ColumnInterface } from 'src/app/shared/types/column.interface';
 import { SocketEventsEnum } from 'src/app/shared/types/socketEvents.enum';
 import { BoardService } from '../../services/board.service';
 
@@ -13,14 +15,18 @@ import { BoardService } from '../../services/board.service';
 })
 export class BoardComponent implements OnInit {
   boardId: string;
-  board$: Observable<BoardInterface>;
+  data$: Observable<{
+    board: BoardInterface;
+    columns: ColumnInterface[];
+  }>;
 
   constructor(
     private boardsService: BoardsService,
     private route: ActivatedRoute,
     private router: Router,
     private boardService: BoardService,
-    private socketService: SocketService
+    private socketService: SocketService,
+    private columnsService: ColumnsService
   ) {
     const boardId = this.route.snapshot.paramMap.get('boardId');
 
@@ -29,7 +35,15 @@ export class BoardComponent implements OnInit {
     }
 
     this.boardId = boardId;
-    this.board$ = this.boardService.board$.pipe(filter(Boolean));
+    this.data$ = combineLatest([
+      this.boardService.board$.pipe(filter(Boolean)),
+      this.boardService.columns$,
+    ]).pipe(
+      map(([board, columns]) => ({
+        board,
+        columns,
+      }))
+    );
   }
 
   ngOnInit(): void {
@@ -52,12 +66,8 @@ export class BoardComponent implements OnInit {
     this.boardsService.getBoard(this.boardId).subscribe((board) => {
       this.boardService.setBoard(board);
     });
-  }
-
-  test(): void {
-    this.socketService.emit('columns:create', {
-      boardId: this.boardId,
-      title: 'foo',
+    this.columnsService.getColumns(this.boardId).subscribe((columns) => {
+      this.boardService.setColumns(columns);
     });
   }
 }
