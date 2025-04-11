65c335096e82bf824af5543d27d56f4bc53b29f8
Akhil Goyal <akhilgoyal@Akhils-MacBook-Pro.local>
2025-04-11 10:43:43 -0400

    create column with socket

--- client/src/app/board/components/board/board.component.ts
+++ client/src/app/board/components/board/board.component.ts
@@ -53,4 +53,11 @@ export class BoardComponent implements OnInit {
       this.boardService.setBoard(board);
     });
   }
+
+  test(): void {
+    this.socketService.emit('columns:create', {
+      boardId: this.boardId,
+      title: 'foo',
+    });
+  }
 }
